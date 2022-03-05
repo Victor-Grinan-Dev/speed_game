bd2fa34f832d96f1fd15a418b5a3a839cbdf05f8
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-03-05 23:05:26 +0200

    standarizing for erd time all files

--- common_main.js
+++ common_main.js
@@ -3,27 +3,42 @@
 const menuBar = document.querySelector("#menuBar");
 const upButton = document.querySelector("#up");
 const welcome = document.querySelector("#welcome");
-
+const mobButton = document.querySelector("#mobileButton");
+const nav = document.querySelector("nav");
+let links = document.querySelectorAll("nav ul li a");
 window.onscroll = () => scrollFunction()
-
-const pageTop = () => {
-
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
 
 function scrollFunction() {
     if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
         menuBar.classList.add("scrollUp");
         upButton.classList.remove("invisible");
         welcome.classList.remove("invisible");
+        mobButton.classList.remove("scrollUp");
         
     } else {  
         menuBar.classList.remove("scrollUp"); 
-        upButton.classList.add("invisible");    
+        upButton.classList.add("invisible"); 
+        mobButton.classList.add("scrollUp");   
     }
   }
 
-upButton.addEventListener("click", pageTop);
+const pageTop = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
+const mobMenu = () => {
+    for (let i = 0; i < links.length; i++) {
+      links[i].addEventListener("click", mobMenu);
+    }
+    if (nav.classList.contains("responsive")) {
+      nav.classList.remove("responsive");
+      document.body.style.overflow = "";
+    } else {
+      nav.classList.add("responsive");
+      document.body.style.overflow = "hidden";
+    }
+  };
 
+  mobButton.addEventListener("click", mobMenu);
+  upButton.addEventListener("click", pageTop);
